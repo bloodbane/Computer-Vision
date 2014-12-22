03d6dc9456bb51921ca8a1901a715f95ff4883dc
tanis <tanis-y@hotmail.com>
2014-12-22 01:24:00 -0500

    modify disparity compute.

--- lab4/convert2node.m
+++ lab4/convert2node.m
@@ -8,10 +8,10 @@ function nodeLabel = convert2node(UorV, x, y, ncols, nrows)
 %         nrows: height of graph
 %   output: corresponding node label
 %   For example a 2*3 graph can be converted into
-%     v -->    10 11 12
-%     v -->    7  8  9
-%     u -->    4  5  6
 %     u -->    1  2  3
+%     u -->    4  5  6
+%     v -->    7  8  9
+%     v -->    10 11 12
 %
     nodeLabel = (y - 1) * ncols + x;
     if UorV == 'v'
---
+++ lab4/path2disparity.m
@@ -0,0 +1,30 @@
+function  disparity = path2disparity(labels, nPixel)
+% map from left image to right image
+    hascut = zeros(nPixel, 1);
+    disparity = zeros(1, nPixel);
+    for i = 1 : nPixel
+        for j = 1 : nPixel
+            idx_u = uv2node('u', i, j, nPixel, nPixel);
+            idx_v = uv2node('v', i, j, nPixel, nPixel);
+            if labels(idx_u) ~= labels(idx_v)
+                hascut(i, 1) = 1;
+                disparity(i) = i - j;
+                break;
+            end
+        end
+        if hascut(i, 1) == 0
+            disparity(i) = inf; % this means occlusion!!
+        end
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
